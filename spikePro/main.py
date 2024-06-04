0d1cb7f0d487bf5beb53218e7f0ac8795e8fe250
Alfon New Mac <hynrbf@gmail.com>
2024-06-04 17:45:38 +0800

    good1

--- spikePro/main.py
+++ spikePro/main.py
@@ -1,13 +1,17 @@
 from hub import light_matrix
 import runloop
-from app import display
+from app import display, sound
+import time
 
 
 class DisplayController:
     @staticmethod
     async def display_text():
-        await light_matrix.write("Hi Grachi!")
-        display.image(4)
+        while True:
+            # await light_matrix.write("Hi Grachi!")
+            sound.play('Bus Door')
+            time.sleep_ms(1000)
+            # display.image(4)
 
 
 async def main():
