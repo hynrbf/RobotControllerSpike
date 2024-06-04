ed069c31142839a6eb87884d2208f10a05ce976a
Chito <remote.alfon@gmail.com>
2024-06-04 20:12:20 +0800

    good2

--- spikePro/main.py
+++ spikePro/main.py
@@ -1,6 +1,5 @@
 from hub import light_matrix
 import runloop
-from app import display, sound
 import time
 
 
@@ -9,12 +8,11 @@ class DisplayController:
     async def display_text(text: str, sleep_ms: int):
         while True:
             await light_matrix.write(text)
-            sound.play('Bus Door')
             time.sleep_ms(sleep_ms)
 
 
 async def main():
-    await DisplayController.display_text("R", 5000)
+    await DisplayController.display_text("lol", 5000)
 
 
 runloop.run(main())
