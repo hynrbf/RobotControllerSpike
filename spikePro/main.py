5196c24794beb96b1dd4f9a19a6c4dcef21538d4
Chito <remote.alfon@gmail.com>
2024-06-06 08:25:06 +0800

    t2

--- spikePro/main.py
+++ spikePro/main.py
@@ -1,10 +1,26 @@
 import runloop
+import motor
+import time
+from hub import port
 
 
 class GrabberController:
     @staticmethod
+    def reset():
+        current_position = motor.absolute_position(port.A)
+        print("His current position is: ", current_position)
+        motor.run_to_absolute_position(port.A, 0, 100, direction=motor.SHORTEST_PATH)
+
+        time.sleep_ms(500)
+
+        current_position = motor.absolute_position(port.A)
+        print("His current position now: ", current_position)
+
+    @staticmethod
     def spin_clockwise():
         print('clockwise')
+        current_position = motor.absolute_position(port.A)
+        print(current_position)
 
     @staticmethod
     def spin_counter_clockwise():
@@ -12,8 +28,9 @@ class GrabberController:
 
 
 async def main():
-    GrabberController.spin_clockwise()
-    GrabberController.spin_counter_clockwise()
+    GrabberController.reset()
+    # GrabberController.spin_clockwise()
+    # GrabberController.spin_counter_clockwise()
 
     # ignore below
     await runloop.sleep_ms(1000)
