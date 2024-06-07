2a89c5126073d62dc900277ee24321ad8ee5bbe5
Chito <remote.alfon@gmail.com>
2024-06-07 14:49:29 +0800

    w1

--- pybricks/pyBricks1/grippercontroller.py
+++ pybricks/pyBricks1/grippercontroller.py
@@ -7,6 +7,7 @@ from shared import Shared
 
 class GripperController:
     __right_motor = Motor(Port.B)
+    __left_motor = Motor(Port.A)
 
     @staticmethod
     def reset_right_arm():
@@ -14,7 +15,7 @@ class GripperController:
         GripperController.__get_right_arm_angle()
         wait(100)
 
-        GripperController.__right_motor.reset_angle(0)
+        GripperController.__right_motor.run_target(500, 0)
         wait(100)
 
         GripperController.__get_right_arm_angle()
@@ -36,8 +37,26 @@ class GripperController:
         GripperController.__get_right_arm_angle()
 
     @staticmethod
-    def grab_element_left_arm():
-        print('left')
+    def reset_left_arm():
+        Shared.hub().display.icon(Icon.EMPTY)
+        GripperController.__get_left_arm_angle()
+        wait(100)
+
+        GripperController.__left_motor.reset_angle(0)
+        wait(100)
+
+        GripperController.__right_motor.run_angle(500, 0, wait=True)
+        wait(100)
+
+        GripperController.__get_left_arm_angle()
+
+    @staticmethod
+    def grip_element_using_left_arm():
+        GripperController.__left_motor.run_angle(500, 270, wait=True)
+        Shared.hub().display.icon(Icon.HEART)
+        wait(100)
+
+        GripperController.__get_left_arm_angle()
 
     @staticmethod
     def grab_element_both_arm():
@@ -47,4 +66,10 @@ class GripperController:
     def __get_right_arm_angle() -> int:
         current_angle = GripperController.__right_motor.angle()
         print("right arm current angle", current_angle)
+        return current_angle
+
+    @staticmethod
+    def __get_left_arm_angle() -> int:
+        current_angle = GripperController.__right_motor.angle()
+        print("left arm current angle", current_angle)
         return current_angle
--- pybricks/pyBricks1/main.py
+++ pybricks/pyBricks1/main.py
@@ -7,7 +7,10 @@ def main():
     # ColorController.get_mat_color(Port.A)
     GripperController.reset_right_arm()
     GripperController.grip_element_using_right_arm()
-    GripperController.release_element_using_right_arm()
+    GripperController.reset_right_arm()
+    # GripperController.release_element_using_right_arm()
+    # GripperController.reset_left_arm()
+    # GripperController.grip_element_using_left_arm()
 
     # ignore below, tester only
     # print("Test simple run")
