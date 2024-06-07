3903e447b4ddb99db394bc2b4e883cee33d65abc
Chito <remote.alfon@gmail.com>
2024-06-07 18:35:06 +0800

    w1

--- pybricks/pyBricks1/main.py
+++ pybricks/pyBricks1/main.py
@@ -1,35 +1,38 @@
-from colorcontroller import ColorController
-from grippercontroller import GripperController
-from pybricks.tools import wait
+# from colorcontroller import ColorController
+# from grippercontroller import GripperController
+# from pybricks.tools import wait
+from wheelcontroller import WheelController
 
 
 def main():
-    while True:
-        # if ColorController.detect_yellow_vegetable():
-        #  GripperController.reset_left_arm()
-        # GripperController.grip_element_using_left_arm()
+    WheelController.move_forward()
 
-        # GripperController.reset_right_arm()
-        # wait(1000)
+    # while True:
+    # if ColorController.detect_yellow_vegetable():
+    #  GripperController.reset_left_arm()
+    # GripperController.grip_element_using_left_arm()
 
-        if ColorController.detect_red_vegetable():
-            GripperController.reset_right_arm()
-            GripperController.grip_element_using_right_arm()
+    # GripperController.reset_right_arm()
+    # wait(1000)
 
-            GripperController.reset_right_arm()
-            wait(1000)
-# GripperController.reset_left_arm()
-# GripperController.reset_right_arm()
-# GripperController.reset_right_arm()
-# GripperController.grip_element_using_right_arm()
-# GripperController.reset_right_arm()
-#
-# GripperController.reset_left_arm()
-# GripperController.grip_element_using_left_arm()
-# GripperController.reset_left_arm()
+    # if ColorController.detect_red_vegetable():
+    #     GripperController.reset_right_arm()
+    #     GripperController.grip_element_using_right_arm()
+    #
+    #     GripperController.reset_right_arm()
+    #     wait(1000)
+    # GripperController.reset_left_arm()
+    # GripperController.reset_right_arm()
+    # GripperController.reset_right_arm()
+    # GripperController.grip_element_using_right_arm()
+    # GripperController.reset_right_arm()
+    #
+    # GripperController.reset_left_arm()
+    # GripperController.grip_element_using_left_arm()
+    # GripperController.reset_left_arm()
 
-# ignore below, tester only
-# print("Test simple run")
+    # ignore below, tester only
+    # print("Test simple run")
 
 
 main()
---
+++ pybricks/pyBricks1/wheelcontroller.py
@@ -0,0 +1,16 @@
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+
+
+class WheelController:
+    __left_motor = Motor(Port.F)
+
+    @staticmethod
+    def move_forward():
+        WheelController.__left_motor.run(500)
+        wait(1500)
+
+    @staticmethod
+    def move_backward():
+        print("backward")
