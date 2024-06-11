c3b20dcf763f1c64e18b50e7ad0f5a3bb0bad5d8
Chito <remote.alfon@gmail.com>
2024-06-11 20:52:46 +0800

    p1

--- pybricks/pyBricks1/shared.py
+++ pybricks/pyBricks1/shared.py
@@ -1,6 +1,6 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
-from pybricks.robotics import DriveBase, GyroDriveBase
+from pybricks.robotics import DriveBase
 
 
 class Speed:
@@ -21,7 +21,8 @@ class Shared:
     def wheels_with_gyro(left_motor: Motor, right_motor: Motor, wheel_diameter_in_mm: float,
                          axle_track_in_mm: float) -> DriveBase:
         if Shared.__wheels_with_gyro is None:
-            # https://github.com/pybricks/support/issues/989
+            # using gyro, https://github.com/pybricks/support/issues/989
+            # changes in code https://github.com/pybricks/pybricks-code/blob/19a809b6df06da252d0ddf100a49f2e474b6d7d4/CHANGELOG.md?plain=1#L60
             Shared.__wheels_with_gyro = DriveBase(left_motor, right_motor, wheel_diameter=wheel_diameter_in_mm,
                                                   axle_track=axle_track_in_mm)
             Shared.__wheels_with_gyro.use_gyro(True)
