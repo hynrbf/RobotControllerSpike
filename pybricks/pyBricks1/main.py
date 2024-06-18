0a2c9cf26af59f69a84367ad7755ee98148dcd73
Anton <jbagalacsa42@gmail.com>
2024-06-18 16:46:08 +0800

    a2

--- pybricks/pyBricks1/main.py
+++ pybricks/pyBricks1/main.py
@@ -5,12 +5,7 @@ from pybricks import version
 from pybricks.tools import run_task
 
 
-# Notes:
-# 1) example of moving motors straight. e.g. float(1000) is 1 meter which is 1000mm
-# 2) when getting vegetable, yung bigat could affect the gyro, so make sure to compute distance
-#    via sensing the white color or ibangga sa edge
-async def main():
-    print("Starting, pb version: ", version)
+async def old():
     # await GripperController.grip_element_using_left_arm()
     # await GripperController.reset_left_arm()
     # await GripperController.grip_element_using_right_arm()
@@ -56,4 +51,18 @@ async def main():
     # print("Test simple run")
 
 
+# Notes:
+# 1) example of moving motors straight. e.g. float(1000) is 1 meter which is 1000mm
+# 2) when getting vegetable, yung bigat could affect the gyro, so make sure to compute distance
+#    via sensing the white color or ibangga sa edge
+async def main():
+    print("Starting, pb version: ", version)
+    wheel_to_butt_distance = float(50)
+    await WheelController.move_wheels_backward_in_straight_line(float(180) - wheel_to_butt_distance)
+    await WheelController.wheel_left_turn()
+    await WheelController.move_wheels_forward_in_straight_line(float(90))
+    await WheelController.wheel_right_turn()
+    await WheelController.move_wheels_forward_in_straight_line(float(570))
+    await WheelController.wheel_left_turn()
+    await WheelController.move_wheels_towards_element_then_stop_at_marker()
 run_task(main())
