79edac5a3f2790c8a87f8b84a6f5e94d79bc8ba3
Chito <remote.alfon@gmail.com>
2024-06-20 21:01:54 +0800

    g3

--- pybricks/pyBricks1/_tester.py
+++ pybricks/pyBricks1/_tester.py
@@ -4,17 +4,19 @@ from shared import Speed
 from wheelcontroller import WheelController
 from pybricks import version
 from pybricks.tools import run_task
-from pybricks.tools import wait
+from pybricks.tools import wait, multitask
 
 
 async def main():
     print("Start, pb version: ", version)
-    await WheelController.move_wheels_towards_water_tower_stop_at_brown_marker()
-    await GripperController.hook_element_using_left_arm()
-    await WheelController.move_wheels_backward_in_straight_line(float(100), Speed.Slow)
-    await WheelController.wheel_slight_left_turn(20)
-    await WheelController.move_wheels_forward_in_straight_line(float(100), Speed.Slow)
-    await WheelController.move_wheels_backward_in_straight_line(float(100), Speed.Slow)
+    await WheelController.move_wheels_forward_in_straight_line(float(60))
+    await WheelController.move_wheels_towards_element_then_stop_at_marker()
+    await multitask(WheelController.move_wheels_backward_in_straight_line(float(120)),
+                    GripperController.reset_left_arm())
+    await GripperController.grip_element_using_left_arm()
+    await WheelController.move_wheels_forward_in_straight_line(float(60))
+    await WheelController.move_wheels_towards_element_then_stop_at_marker()
+
     print("DONE!")
 
 
--- pybricks/pyBricks1/main.py
+++ pybricks/pyBricks1/main.py
@@ -2,7 +2,7 @@ from grippercontroller import GripperController
 from shared import Speed
 from wheelcontroller import WheelController
 from pybricks import version
-from pybricks.tools import run_task
+from pybricks.tools import run_task, multitask
 
 
 async def old_code():
@@ -92,6 +92,11 @@ async def main():
     await WheelController.wheel_right_turn_with_angle(float(30))
     await WheelController.move_wheels_forward_in_straight_line(float(300))
     await WheelController.wheel_left_turn()
+    await WheelController.move_wheels_forward_in_straight_line(float(60))
+    await WheelController.move_wheels_towards_element_then_stop_at_marker()
+    await multitask(WheelController.move_wheels_backward_in_straight_line(float(120)),
+                    GripperController.reset_left_arm())
+    await GripperController.grip_element_using_left_arm()
     await WheelController.move_wheels_forward_in_straight_line(float(60))
     await WheelController.move_wheels_towards_element_then_stop_at_marker()
     print("DONE!")
