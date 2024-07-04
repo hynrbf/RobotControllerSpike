e06f3809f9f5e43be7c708983085c078405d5727
Chito <remote.alfon@gmail.com>
2024-07-04 23:11:50 +0800

    S6

--- pybricks/pyBricks1/main.py
+++ pybricks/pyBricks1/main.py
@@ -113,7 +113,7 @@ async def get_the_vegetables():
     await WheelController.wheel_slight_left_turn()
     await WheelController.move_wheels_forward_in_straight_line(float(310))
     await multitask(GripperController.reset_left_arm(),
-                    WheelController.move_wheels_backward_in_straight_line(float(450)))
+                    WheelController.move_wheels_backward_in_straight_line(float(400)))
     await multitask(WheelController.wheel_slight_left_turn(), GripperController.grip_element_using_left_arm())
 
     # going to red market
